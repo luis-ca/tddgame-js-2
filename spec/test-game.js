c3c42fc4a137e5cb8f510fcfd4135e9f541854ca
Leonardo Aretakis <leoaretakis@gmail.com>
2016-09-16 06:14:53 +0100

    on play, attacker attacks

--- spec/test-game.js
+++ spec/test-game.js
@@ -2,6 +2,7 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 
 var Game = require('../src/game');
+var Player = require('../src/player');
 var Die = require('../src/die');
 
 describe('Game', function() {
@@ -9,22 +10,20 @@ describe('Game', function() {
   var player1, player2, game;
 
   beforeEach(function () {
-    player1 = {
-      life: 10
-    };
-
-    player2 = {
-      life: 10
-    };
-
-    game = new Game(player1, player2);
+    player1 = new Player(10);
+    player2 = new Player(10);
+    game = new Game(player1, player2,
+      new Die(8), new Die(12));
   });
 
-  describe('#playRound', function() {
+  describe('on play', function() {
 
-    // game.playRound();
+    it('player1 attacks the defender', function() {
+      player1.attack = sinon.spy();
+      game.play();
 
-    // expect(game.player2.life).to.equal(8);
+      expect(player1.attack.called).to.be.true;
+    });
 
   })
 
--- src/game.js
+++ src/game.js
@@ -1,5 +1,17 @@
-function Game(p1, p2) {
+function Game(
+  player1,
+  player2,
+  attackDie,
+  defenseDie) {
+  this.attacker = player1;
+  this.defender = player2;
 
+  this.attackDie = attackDie;
+  this.attackDie = attackDie;
 }
+
+Game.prototype.play = function() {
+  this.attacker.attack(this.attackDie);
+};
 
 module.exports = Game;
